75851f947bfebd84482ac0b55082d6e1af71af1e
Andrii <radomely@gmail.com>
2019-11-04 22:13:31 +0200

    Session

--- 06_DB/Mongo/004_Find/main.js
+++ 06_DB/Mongo/004_Find/main.js
@@ -1,34 +1,35 @@
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require("mongodb").MongoClient;
 
-var url = 'mongodb://localhost:27017/userCollectionDB';
+var url = "mongodb://localhost:27017";
+const dbName = "userCollectionDB";
+MongoClient.connect(url, function(err, client) {
+  const db = client.db(dbName);
+  var collection = db.collection("users");
 
-MongoClient.connect(url, function(err, db){
-    var collection = db.collection('users');
+  // Метод find возвращает специальный объект - Cursor
+  var cursor = collection.find();
 
-    // Метод find возвращает специальный объект - Cursor
-    var cursor = collection.find();
-    
-    // Метод toArray - все полученные данные преобразовывает в массив и передает в функцию обратного вызова
-    cursor.toArray(function(err, res){
-        console.log('********** All Data **********');
-        console.log(res);
-        console.log('****************************');
-        
-        // Метод find(параметр) - возвращает коллекцию объектов, которые соответсвуют условию
-        collection.find({name : "Sergey"}).toArray(function(err, res){
-            console.log('********** Employees with name Sergey **********');
-            console.log(res);
-            console.log('***********************************************');
-            
-            // Метод findOne - позволяет найти одну запись в коллекции с указанными параметрами
-            collection.findOne({age : {$lt : 30} }).then(function(value){
-                console.log('********** Employee with age less than 30 **********');
-                console.log(value);
-                console.log('***********************************************');
-                
-                // закрываем подключение к базе данных
-                db.close(); 
-            });
-        });
-    });    
+  // Метод toArray - все полученные данные преобразовывает в массив и передает в функцию обратного вызова
+  cursor.toArray(function(err, res) {
+    console.log("********** All Data **********");
+    console.log(res);
+    console.log("****************************");
+
+    // Метод find(параметр) - возвращает коллекцию объектов, которые соответсвуют условию
+    collection.find({ name: "Sergey" }).toArray(function(err, res) {
+      console.log("********** Employees with name Sergey **********");
+      console.log(res);
+      console.log("***********************************************");
+
+      // Метод findOne - позволяет найти одну запись в коллекции с указанными параметрами
+      collection.findOne({ age: { $lt: 30 } }).then(function(value) {
+        console.log("********** Employee with age less than 30 **********");
+        console.log(value);
+        console.log("***********************************************");
+
+        // закрываем подключение к базе данных
+        client.close();
+      });
+    });
+  });
 });
---
+++ 06_DB/Mongo/006_UpdateData/main.js
@@ -0,0 +1,33 @@
+// Для удаления объектов (документов) из коллекции используется несколько методов
+// updateOne: обновляет один документ, который соответствует критерию фильтрации, и возвращает информацию об операции обновления
+// updateMany: обновляет все документы, которые соответствуют критерию фильтрации, и возвращает информацию об операции обновления
+// findOneAndUpdate: обновляет один документ, который соответствует критерию фильтрации, и возвращает обновленный документ
+
+var mongoClient = require("mongodb").MongoClient;
+
+var users = [
+  { name: "Bob", age: 34 },
+  { name: "Alice", age: 25 },
+  { name: "Tom", age: 45 }
+];
+
+mongoClient.connect("mongodb://localhost:27017", function(err, client) {
+  const db = client.db("userCollectionDB");
+  var collection = db.collection("users");
+  collection.insertMany(users, function(err, results) {
+    collection.findOneAndUpdate(
+      // критерий выборки
+      { age: 25 },
+      // параметр обновления
+      { $set: { age: 21 } },
+      // доп. опции обновления
+      {
+        returnOriginal: false
+      },
+      function(err, result) {
+        console.log(result);
+        client.close();
+      }
+    );
+  });
+});
